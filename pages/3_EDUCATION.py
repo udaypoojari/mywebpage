91933ab3ec8839718df9a53729fff1f34eead49a
udaypoojari <109072005+udaypoojari@users.noreply.github.com>
2024-12-15 17:08:34 -0500

    Add files via upload

---
+++ pages/3_EDUCATION.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 18:17:30 2024
+
+@author: every
+"""
+import streamlit as st
+
+import matplotlib.image as image
+
+
+st.sidebar.markdown("# EDUCATION")
+
+
+st.markdown(
+"""
+### GRADUATION: 
+""")    
+
+st.write("""
+    Bachelor of Technology in Mechanical Engineering.\n
+    Jawaharlal Nehru technological University, Hyderabad, India.\n
+    2004-2008.       
+         
+         """)
+
+st.markdown(
+"""
+### POST-GRADUATION: 
+""")    
+
+st.write("""
+    Master of Technology in Computer Aided Design and Manufacturing.\n
+    Jawaharlal Nehru technological University, Hyderabad, India.\n
+    2009-2011.       
+         
+         """)
+        
+
+
+
+
+
+
+
+#my_image = image.imread("C:/Users/every/OneDrive/Documents/UDAY/StreamLit/MyPage/my_pic.jpg")
+
+#st.image(my_image, caption="Me at brunch.")
---
+++ pages/4_EXPERIENCE.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 18:17:30 2024
+
+@author: every
+"""
+import streamlit as st
+
+import matplotlib.image as image
+
+
+st.sidebar.markdown("# WORK EXPERIENCE")
+
+# st.image("https://via.placeholder.com/150", caption="Sample Image")
+
+st.markdown(
+"""
+#### FORD MOTOR COMPANY, Dearborn, USA.                        
+""")    
+st.markdown(
+"""##### Software Developer, Global power train, chassis and suspension.                        
+""") 
+
+
+with st.expander("Click to see my work at FORD"):
+    st.markdown(
+    """###### Duration:                        
+    """)
+    st.write("May,2023 to till date.")
+    st.markdown(
+    """###### Projects:                        
+    """)
+    st.write("Designed and Developed applications to aid CAE simulations run on HPC and workstations.")
+    st.write("Developed socket query tool to establish interaction between client and server.")
+    st.write("Architected and Developed application to build 1-Dimensional finite element model using ADAMS car data to reduce 60% of project timeline.") 
+    st.write("Built Machine learning models to predict brake temperatures under various test conditions during initial design phase to expedite the brake design process.")
+    st.write("Integrated machine learning model into BEAST (inhouse brakes) tool using ONNX runtime framework. ")
+    st.write("Extended Hypermesh application using HM_EXTAPI to handle finite element data collected from various sources.")
+    st.write("Provide solutions to engineers with their data handling, data processing and manipulating problems by developing multiple windows-based applications using C# with .net framework, C++, winform, MFC, python, VBA and bash.") 
+    st.write("Developed Macros to automate pre and post-processing of finite element analysis and setup the optimization studies and achieve efficiency greater than 70%.")
+    st.write("Automated Duty cycle load generation process within Ncode and reduced wait time of dependent engineers around 18 hours. ")
+    st.write("Developed and deployed project tracking Dashboards using Qliksense for management and supervisors.")
+
+
+
+st.markdown(
+"""
+#### DASSAULT SYSTEMES, Pune, India.                        
+""")    
+st.markdown(
+"""##### Software Enginnering Manager, SIMULIA.                        
+""") 
+
+
+with st.expander("Click to see my work at Dassault"):
+    st.markdown(
+    """###### Duration:                        
+    """)
+    st.write("Nov,2020 to May,2023.")
+    st.markdown(
+    """###### Projects:                        
+    """)
+    st.write("Analyzed, developed and maintained model attribute features of 3D SIMULIA application.")
+    st.write("Enhanced application and improved app compatibility with other external apps.")
+    st.write("Data model changes to create or edit features.")
+    st.write("Created ODTs (unit tests) to improve the code coverage.")
+    st.write("Performed UI enhancements to allow edit of volatile features.")
+    st.write("Resolving framework cyclic dependencies.")
+    st.write("Create Product Enhancement specification (PES) for the functions to be delivered.")
+    st.write("Managing release of team workspace for functional deliveries.")
+    st.write("Handling and fixing the internal and external incidents (bugs) reported.")
+    st.write("Documentation of the developed functions for user assistance.")
+
+
+st.markdown(
+"""
+#### DEP India Pvt Ltd, Chennai, India.                        
+""")    
+st.markdown(
+"""##### Senior Software Enginner, MeshWorks.                        
+""") 
+
+
+with st.expander("Click to see my work at DEP"):
+    st.markdown(
+    """###### Duration:                        
+    """)
+    st.write("March,2019 to Oct,2020.")
+    st.markdown(
+    """###### Projects:                        
+    """)
+    st.write("Development and integration of Meshworks FEM application.")
+    st.write("Handled import, export functionalities of Meshworks.")
+    st.write("Lead the Abaqus import export functionality and successfully deployed.")
+    st.write("Bug fixing and functional enhancements.")
+    st.write("Securing the code with unit test cases.")
+    st.write("Release management.") 
+    st.write("Profiling and optimizing code performance.")
+
+st.markdown(
+"""
+#### Allegis Services India Pvt Ltd (Deputed to FORD), Chennai, India.                        
+""")    
+st.markdown(
+"""##### Senior CAE Analyst, Chassis and Suspension.                        
+""") 
+
+
+with st.expander("Click to see my work at FORD India"):
+    st.markdown(
+    """###### Duration:                        
+    """)
+    st.write("Oct,2015 to March,2019.")
+    st.markdown(
+    """###### Projects:                        
+    """)
+    st.write("Conducted chassis system- and component-level durability analyses to validate designs through CAE simulations.")
+    st.write("Overlook the CAE model built, and qualities delivered.")
+    st.write("Standardizing and automating the CAE process.")
+    st.write("Performed nonlinear, dynamic and fatigue analysis of chassis components to correlate them with test data and optimize their design if required.")
+    st.write("Knuckle tilting Bearing gap calculation (calculating gap between the knuckle and bearing interference with the effect of cornering loads).")
+    st.write("Lower ball joint bolt Torque Loss calculation.")
+    st.write("Bolt slippage calculation of fasteners in an assembly.")
+    st.write("Skid plate impact analysis.")
+    st.write("Global static strength (GSS) analysis and buckling load calculation of Control Arm.")
+    st.write("Substructuring of components (Body stiffness matrix extraction).")
+    st.write("Rear twist beam design support and improvisation.")
+
+
+st.markdown(
+"""
+#### Hyundai Mobis India Pvt Ltd, Hyderabad, India.                        
+""")    
+st.markdown(
+"""##### Research Engineer                        
+""") 
+
+
+with st.expander("Click to see my work at Mobis"):
+    st.markdown(
+    """###### Duration:                        
+    """)
+    st.write("Aug,2011 to Sep,2015.")
+    st.markdown(
+    """###### Projects:                        
+    """)
+    st.write("Performed strength, stiffness and fatigue estimations of chassis components and system using CAE simulations.")
+    st.write("Weld and base metal fatigue analysis.")
+    st.write("Vibration Fatigue analysis of components and systems.")
+    st.write("Supported major car production release events from Seoul, South Korea.")
+    st.write("Calculating Non-linear Stiffness of rubber bush.")	
+    st.write("Vibration Fatigue analysis of a Headlamp to predict its life.")
+    st.write("Strength and stiffness calculation of battery pack assembly.")	
+    st.write("Finite element modeling and assembling of battery pack, headlamp and cockpit modules.")	
+
+
+
+
+
+
+#my_image = image.imread("C:/Users/every/OneDrive/Documents/UDAY/StreamLit/MyPage/my_pic.jpg")
+
+#st.image(my_image, caption="Me at brunch.")
