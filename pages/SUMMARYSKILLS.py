6a8988c82216baf136a6efb644f3eb7b8d64c325
udaypoojari <109072005+udaypoojari@users.noreply.github.com>
2024-12-16 17:47:26 -0500

    Add files via upload

---
+++ pages/SUMMARYSKILLS.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 18:17:30 2024
+
+@author: every
+"""
+import streamlit as st
+def main():
+    st.sidebar.markdown("# PROFESSIONAL SUMMARY")
+    
+    
+    st.markdown(
+    """
+    ### PROFESSIONAL SUMMARY: 
+    """)    
+    
+    st.write("""
+        - 13 years of experience in Design & Development of applications, Analytics, UI Frameworks and product development with deep insight into Automotive and Aerospace domain.\n
+        - Experience with advanced level of programming in C++, C# and python for windows and Linux platforms.\n
+        - Extensive involvement in design and implementation of client/server application.\n
+        - Proficient with CAE Durability calculations.\n
+        - Possess deep insight and experience with product development and entire software development life cycle (SDLC).\n
+        - Experience with building and deploying machine learning models.\n
+        - Proven expertise in streamlining and automating processes for improved consistency and efficiency.\n
+        - Practical experience working with Component Object Model (COM) architecture.\n
+        - Developed and deployed dashboards using business analytics tools such as qliksense and tableau.\n
+        - Handled release management and managed team workspace images.\n
+        - Created Product Enhacement specification(PES) Documentation for the developed functionalities.\n         
+            
+            
+            """)
+        
+    
+    st.markdown(
+    """
+    ### SKILLS:    
+        * C++                        * Python        
+        * C#                         * TCL           
+        * SHELL                      * Qliksense     
+        * Azure                      * Automation    
+        * Machine Learning           * Abaqus
+        * GIT                        * Nastran
+        * VBA                        * Design life Ncode
+        * SQL                        * Hyperworks and Hypermesh
+        * Tableau                    * Data Analytics
+        * Software development       * Code profiling and optimization
+        * Product development        * Design optimization
+        * Unit testing               * Data Engineering
+                    
+    """)
+    
+    
+
+    
+    #my_image = image.imread("C:/Users/every/OneDrive/Documents/UDAY/StreamLit/MyPage/my_pic.jpg")
+    
+    #st.image(my_image, caption="Me at brunch.")
+    
