e7562469bc17a66669c2f748cce62a93e9c68e7a
udaypoojari <109072005+udaypoojari@users.noreply.github.com>
2024-12-22 21:05:28 -0500

    Add files via upload

---
+++ pages/5_Contact.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 18:17:30 2024
+
+@author: every
+"""
+import streamlit as st
+
+
+st.sidebar.markdown("# CONTACT DETAILS")
+
+# st.image("https://via.placeholder.com/150", caption="Sample Image")
+
+st.markdown(
+"""
+#### Feel free to reach out to me.                        
+""")    
+st.markdown(
+"""##### Email-id:                        
+""") 
+
+st.write("udaykumar405@gmail.com.")
+
+st.markdown(
+"""##### Mobile Number:                        
+""") 
+
+st.write("+1 734-643-2533")
+
+st.markdown(
+"""##### Linkedin profile:                        
+""") 
+
+st.write("https://www.linkedin.com/in/uday-poojari-33884734")
+
+st.markdown(
+"""
+
+#### Have a great day! Keep working out 💪, keep learning 📚 and Keep smiling  😊 ....                      
+""") 
+
+st.markdown(
+"""
+
+#### [Prev Page <--](https://udaypoojari-405.streamlit.app/EXPERIENCE)
+""")
+
+
+
